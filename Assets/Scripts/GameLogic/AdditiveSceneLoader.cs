be4bfffc8b78ecc337d88e18d3e623eb34e009ce
Arnaud Briche <briche@absolute.local>
2020-01-31 17:57:55 +0100

    Fixing input error, finished ProjectSettings, Setup Lighting and PP

---
+++ Assets/Scripts/GameLogic/AdditiveSceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GGJ.GameLogic
+{
+    public class AdditiveSceneLoader : MonoBehaviour
+    {
+        [SerializeField] private string[] _additiveScenesNames;
+
+        private void Awake()
+        {
+            foreach (var scene in _additiveScenesNames)
+            {
+                SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+            }
+        }
+    }
+}
