4c1057997f794ee0adc1b176513b99390674b1ac
Arnaud Briche <briche@absolute.local>
2020-02-01 12:29:10 +0100

    Added basic hand animation, finish tutorial logic

---
+++ Assets/Scripts/Tutorial/TutorialObjectDeactivator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace GGJ.Tutorial
+{
+    /// <summary>
+    /// When place on a tutorial object, deactivate it once we pass the specified step
+    /// </summary>
+    public class TutorialObjectDeactivator : MonoBehaviour
+    {
+        [Header("Tutorial step on which the object is deactivated")]
+        [SerializeField] private ETutorialSteps _deactivateAfter;
+
+        private void Awake()
+        {
+            OnTutorialStepDone.Listeners += CheckNewTutoStep;
+        }
+
+        private void CheckNewTutoStep(OnTutorialStepDone info)
+        {
+            if (info.TutorialStepDone == _deactivateAfter)
+            {
+                gameObject.SetActive(false);
+                OnTutorialStepDone.Listeners -= CheckNewTutoStep;
+            }
+        }
+    }
+}
